fa75207119428890e90ef444a4a2461bf199c2cc
edrakopo <edrakopo@staffmail.ed.ac.uk>
2020-11-26 14:35:35 +0000

    keras_classification

--- DNN_Classification/DNNClassifier_ANNIESignal_Bkgd.py
+++ DNN_Classification/DNNClassifier_ANNIESignal_Bkgd.py
@@ -8,7 +8,8 @@ import tempfile
 import random
 import csv
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from array import array
 from sklearn import datasets
@@ -29,9 +30,11 @@ from sklearn.pipeline import Pipeline
 
 #--------- File with events for reconstruction:
 #--- evts for training:
-infile = "/Users/edrakopo/work/ANNIETools/ANNIENtupleAnalysis/util/vars_DNN_Signal_Bkgd2.csv"
-infile0 = "/Users/edrakopo/work/ANNIETools/ANNIENtupleAnalysis/util/labels_DNN_Signal_Bkgd.csv"
-#
+#infile = "/Users/edrakopo/work/ANNIETools/ANNIENtupleAnalysis/util/vars_DNN_Signal_Bkgd2.csv"
+#infile0 = "/Users/edrakopo/work/ANNIETools/ANNIENtupleAnalysis/util/labels_DNN_Signal_Bkgd.csv"
+#---protmpt evts for signal:
+infile = "/Users/edrakopo/work/ANNIETools/ANNIENtupleAnalysis/util/vars_DNN_Signal_Bkgd_prompt2.csv"
+infile0 = "/Users/edrakopo/work/ANNIETools/ANNIENtupleAnalysis/util/labels_DNN_Signal_Bkgd_prompt.csv"
 
 # Set TF random seed to improve reproducibility
 seed = 150
@@ -39,39 +42,36 @@ np.random.seed(seed)
 
 print( "--- opening file with input variables!")
 #--- events for training - MC events: variables
-#df = pd.read_csv(infile)#,names=range(1000))
 #print("All columns are: ", df.columns.values.tolist())
 #filein = open(str(infile))
 #print("evts for training in: ",filein)
-#df=pd.read_csv(infile)
 data = pd.read_csv(infile,header=None,names=range(1500)).fillna(0)
 print(data.head())
-trainX = np.array(data[:300])
-#X = np.array(df.drop(['label'], axis=1))
-#X, rest = np.split(Dataset,[5],axis=1)
-#X = df[:,0:8]#.astype(float)
+trainX = np.array(data[:3000]) #variables to train
+testX = np.array(data[3000:]) #variables to test
 print("trainX[0]: ",trainX[0])
 print(trainX.shape)
-data0 = np.array(pd.read_csv(infile0)[:300])
-rest, Y = np.split(data0,[6],axis=1)
+#print("All columns are: ", data.columns.values.tolist())
+
+dataY00 = pd.read_csv(infile0)
+dataY0 = np.array(dataY00[:3000])
+rest, Y = np.split(dataY0,[6],axis=1)
 print("before Y.shape ",Y.shape)
 Y=Y.reshape(-1)
 print("after: ",Y.shape)
 print("Y: ",Y[0])
-'''
-#split events in train/test samples:
-num_events, num_pixels = features.shape
-print(num_events, num_pixels)
-np.random.seed(0)
-train_x = features[:1000]
-train_y = labels[:1000]
-print("train sample features shape: ", train_x.shape," train sample label shape: ", train_y.shape)
-'''
+
+dataYtest = np.array(dataY00[3000:])
+rest1, Ytest = np.split(dataYtest,[6],axis=1)
+Ytest=Ytest.reshape(-1)
+
 # Scale data (training set) to 0 mean and unit standard deviation.
 scaler = preprocessing.StandardScaler()
 X = scaler.fit_transform(trainX)
 print("X.shape: ", X.shape)
 print("types: ",type(X), " ",type(Y))
+Xtest = scaler.fit_transform(testX)
+print("testX.shape: ", testX.shape," Ytest.shape ",Ytest.shape)
 
 '''
 # load dataset
@@ -86,6 +86,7 @@ print("types1: ",type(X1), " ",type(Y1))
 print("X1.shape ",X1.shape)
 print("Y1.shape ",Y1.shape)
 '''
+
 # encode class values as integers
 encoder = LabelEncoder()
 encoder.fit(Y)
@@ -94,21 +95,30 @@ encoded_Y = encoder.transform(Y)
 def create_model():
     # create model
     model = Sequential()
-    model.add(Dense(800, input_dim=1500, activation='relu'))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(30, activation='relu'))
+    #model.add(Dense(800, input_dim=1500, activation='relu'))
+    #model.add(Dense(100, activation='relu'))
+    #model.add(Dense(30, activation='relu'))
+    model.add(Dense(50, input_dim=1500, activation='relu'))
+    model.add(Dense(25, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
+
+#keras_model = create_model()
+#keras_model.fit(trainX, Y, epochs=100, batch_size=10, verbose=1)
+estimator = KerasClassifier(build_fn=create_model, epochs=20, batch_size=5, verbose=0)
+estimator.fit(X,encoded_Y,verbose=0)
 '''
 #evaluate this model using stratified cross validation in the scikit-learn
-estimator = KerasClassifier(build_fn=create_model, epochs=100, batch_size=5, verbose=0)
+#estimator = KerasClassifier(build_fn=create_model, epochs=100, batch_size=5, verbose=0)
 kfold = StratifiedKFold(n_splits=10, shuffle=True)
 results = cross_val_score(estimator, X, encoded_Y, cv=kfold)
 print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+#pred_results = cross_val_score(estimator, Xtest, Ytest, cv=kfold)
+#print("Baseline: %.2f%% (%.2f%%)" % (pred_results.mean()*100, pred_results.std()*100))
 '''
-
+'''
 # evaluate baseline model using a pipeline with: i)stratified cross validation and ii)a standardized dataset
 estimators = []
 estimators.append(('standardize', StandardScaler()))
@@ -117,5 +127,54 @@ pipeline = Pipeline(estimators)
 kfold = StratifiedKFold(n_splits=10, shuffle=True)
 results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
 print("Standardized: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+'''
+# checkpoint
+filepath="weights_bets.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=True, mode='auto')
+callbacks_list = [checkpoint]
+# Fit the model
+history = estimator.fit(X, encoded_Y, validation_split=0.33, epochs=20, batch_size=5, callbacks=callbacks_list, verbose=0)
 
+# summarize history for loss
+f, ax2 = plt.subplots(1,1)
+ax2.plot(history.history['accuracy'])
+ax2.plot(history.history['val_accuracy'])
+ax2.set_title('model accuracy')
+ax2.set_ylabel('Performance')
+ax2.set_xlabel('Epochs')
+#ax2.set_xlim(0.,10.)
+ax2.legend(['train', 'test'], loc='upper left')
+plt.savefig("keras_train_test.pdf")
+
+#ROC curve:
+from sklearn.metrics import roc_curve
+
+Ypred = estimator.predict(Xtest).ravel()
+Ypred =np.vstack(Ypred)
+Ytest=np.vstack(Ytest)
+#store predictions to csv:
+df1 = pd.DataFrame(data=Ytest,columns=['Yvalues'])
+df2 = pd.DataFrame(data=Ypred,columns=['Prediction'])
+print("check df head: ",df1.head()," ", df2.head())
+df = pd.concat([df1,df2], axis=1)
+print(df.head())
+df.to_csv("predictionsKeras.csv")
+
+#print("Ytest: ",Ytest[:10]," and predicts ", Ypred[:10])
+#print(type(Xtest)," ",type(Ytest)," Ytest.shape ",Ytest.shape," Ypred.shape ",Ypred.shape)
+fpr_keras, tpr_keras, thresholds_keras = roc_curve(Ytest, Ypred)
+#AUC:
+from sklearn.metrics import auc
+
+auc_keras = auc(fpr_keras, tpr_keras)
+
+f1, ax1 = plt.subplots(1,1)
+ax1.plot([0, 1], [0, 1], 'k--')
+ax1.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
+ax1.set_xlabel('False positive rate')
+ax1.set_ylabel('True positive rate')
+ax1.set_title('ROC curve')
+ax1.legend(loc='best')
+plt.savefig("ROC_curve.pdf")
+#plt.show()
 
