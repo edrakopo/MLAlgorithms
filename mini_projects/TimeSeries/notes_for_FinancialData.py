c45b5c92c3bc3190cb332b6eabc421cd97cb4392
Evangelia Drakopoulou <edrakopo@staffmail.ed.ac.uk>
2022-02-25 23:51:02 +0200

    Delete mini_projects directory

--- mini_projects/TimeSeries/notes_for_FinancialData.py
+++
@@ -1,2 +0,0 @@
-https://towardsdatascience.com/time-series-analysis-in-python-an-introduction-70d5a5b1d52a
-
--- mini_projects/TimeSeries/notes_for_salesData.py
+++
@@ -1,1 +0,0 @@
-https://towardsdatascience.com/an-end-to-end-project-on-time-series-analysis-and-forecasting-with-python-4835e6bf050b
